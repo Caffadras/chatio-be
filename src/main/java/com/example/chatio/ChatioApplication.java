6db855e82f81c469e5e93ce0e75ec6e7b33bbe9c
Dubrovschii Semion <dubrovschiisemion@gmail.com>
2023-03-14 21:00:40 +0200

    init

---
+++ src/main/java/com/example/chatio/ChatioApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatioApplication.class, args);
+    }
+
+}
